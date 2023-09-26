4d8ac6efd388917a2e92aaaafd7868f61706a267
sam-xander <sam@samxander.com>
2023-09-27 00:26:50 +0100

    updated images with webp

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -2,103 +2,69 @@ import PhotoAlbum from "react-photo-album";
 import Services from "../components/Services";
 
 function Hero() {
-  // Function to build Cloudinary URLs
-  const buildCloudinaryURL = (imageName, width, height) => {
-    return `https://res.cloudinary.com/dzqnbbola/image/fetch/q_auto,f_auto,w_${width},h_${height}/v1695763569/https://samxander.com/cld-assets/images/gallery/${imageName}`;
-  };
-
   const images = [
     {
-      src: buildCloudinaryURL("modern-website-concept-1.jpg", 1920, 1080),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768421/modern-website-concept-1.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: buildCloudinaryURL("creative-website-concept-1.png", 1920, 1080),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768373/creative-website-concept-1.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: buildCloudinaryURL("Cypher-window-min.png", 1903, 960),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768376/Cypher-window-min.webp",
       width: 1903,
       height: 960,
     },
     {
-      src: buildCloudinaryURL("artist-portfolio-1.png", 1920, 1080),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768375/cypher-one-1.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: buildCloudinaryURL("cypher-one-1.png", 1920, 1080),
-      width: 1920,
-      height: 1080,
-    },
-    {
-      src: buildCloudinaryURL("playtime-1.png", 1920, 1080),
-      width: 1920,
-      height: 1080,
-    },
-    {
-      src: buildCloudinaryURL("get-assessed-1-min.png", 1654, 2339),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768379/get-assessed-1-min.webp",
       width: 1654,
       height: 2339,
     },
     {
-      src: buildCloudinaryURL("cypher-features-min.png", 1920, 1080),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768375/cypher-features-min.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: buildCloudinaryURL("cypher-project-1.png", 2005, 2629),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768376/cypher-project-1.webp",
       width: 2005,
       height: 2629,
     },
     {
-      src: buildCloudinaryURL(
-        "cypher-dashboard-announcement copy-min.png",
-        1920,
-        1080
-      ),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768375/cypher-dashboard-announcement_copy-min.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: buildCloudinaryURL("Crowdsurf-report.png", 1654, 2339),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768374/Crowdsurf-report.webp",
       width: 1654,
       height: 2339,
     },
     {
-      src: buildCloudinaryURL("10k Followers-min.png", 1600, 900),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768361/10k_Followers-min.webp",
       width: 1600,
       height: 900,
     },
     {
-      src: buildCloudinaryURL("artist-portfolio-2.png", 1920, 2312),
-      width: 1920,
-      height: 2312,
-    },
-    {
-      src: buildCloudinaryURL("banner 1_alt-min.png", 1232, 1552),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768372/banner_1_alt-min.webp",
       width: 1232,
       height: 1552,
     },
     {
-      src: buildCloudinaryURL("2K-followers-min.png", 1080, 1920),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768421/2K-followers-min.webp",
       width: 1080,
       height: 1920,
     },
     {
-      src: buildCloudinaryURL("1.5K-followers-min.png", 1080, 1920),
-      width: 1080,
-      height: 1920,
-    },
-    {
-      src: buildCloudinaryURL("details_2_alt-min.jpg", 1232, 1352),
-      width: 1232,
-      height: 1352,
-    },
-    {
-      src: buildCloudinaryURL("details_3_alt-min.jpg", 1232, 1352),
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768377/details_3_alt-min.webp",
       width: 1232,
       height: 1352,
     },
