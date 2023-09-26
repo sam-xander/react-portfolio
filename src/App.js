c2b70621dc81c6af13fe19befea19a1822975a16
sam-xander <sam@samxander.com>
2023-09-26 22:17:22 +0100

    remove pages declaration

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Hero from "./pages/Hero";
-import Work from "./pages/Work";
-import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 
 function App() {
@@ -15,8 +13,6 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Hero />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
 
