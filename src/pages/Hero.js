926c52bb9bcdacae5d8fb3ba45012e8ea93eab04
sam-xander <sam@samxander.com>
2023-09-27 01:24:16 +0100

    optimise images

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -4,67 +4,67 @@ import Services from "../components/Services";
 function Hero() {
   const images = [
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695772307/Capture-Everything_c6uvlm.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:good/v1695772307/Capture-Everything_c6uvlm.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768373/creative-website-concept-1.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695768373/creative-website-concept-1.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768376/Cypher-window-min.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695768376/Cypher-window-min.webp",
       width: 1903,
       height: 960,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768375/cypher-one-1.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695768375/cypher-one-1.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768379/get-assessed-1-min.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695768379/get-assessed-1-min.webp",
       width: 1654,
       height: 2339,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768375/cypher-features-min.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695768375/cypher-features-min.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695773013/cypher-project-page_dqxzeb.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695773013/cypher-project-page_dqxzeb.webp",
       width: 2005,
       height: 2629,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768375/cypher-dashboard-announcement_copy-min.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695768375/cypher-dashboard-announcement_copy-min.webp",
       width: 1920,
       height: 1080,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695771945/Crowdsurf-report_jtmbjh.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695771945/Crowdsurf-report_jtmbjh.webp",
       width: 1654,
       height: 2339,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768361/10k_Followers-min.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695768361/10k_Followers-min.webp",
       width: 1600,
       height: 900,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695772747/banner_1_alt_h0xoyu.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695772747/banner_1_alt_h0xoyu.webp",
       width: 1232,
       height: 1552,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768421/2K-followers-min.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695768421/2K-followers-min.webp",
       width: 1080,
       height: 1920,
     },
     {
-      src: "https://res.cloudinary.com/dzqnbbola/image/upload/v1695768377/details_3_alt-min.webp",
+      src: "https://res.cloudinary.com/dzqnbbola/image/upload/f_auto,q_auto:low/v1695768377/details_3_alt-min.webp",
       width: 1232,
       height: 1352,
     },
